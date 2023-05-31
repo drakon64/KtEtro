0718df51b27deaec5ccf5c3c01b3d682c1d9499c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-05-31 22:41:04 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+}
+
+group = "cloud.drakon"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
